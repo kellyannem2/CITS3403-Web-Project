063430008fffb107d98344f82669e7fd68d416da
kelly <kellyannemccabe02@gmail.com>
2025-05-07 17:37:05 +0800

    added js changes to updated main

--- app/static/js/exercise_log.js
+++ app/static/js/exercise_log.js
@@ -2,25 +2,25 @@
 // Adding exercises in dynamically from the Exercises Page
 
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("Script is loaded");
-  
-  const logModal = document.getElementById("logModal");
-  const openLogModalBtn = document.getElementById("openLogModalBtn");
-  const closeLogModalBtn = document.getElementById("closeLogModal");
-  
-  if (openLogModalBtn && logModal && closeLogModalBtn) {
-    openLogModalBtn.onclick = () => {
-      logModal.style.display = "block";
-    };
-  
-    closeLogModalBtn.onclick = () => {
-      logModal.style.display = "none";
-    };
-  
-    window.onclick = function (event) {
-      if (event.target == logModal) {
-        logModal.style.display = "none";
+    console.log("Exercise modal script loaded");
+
+    const exerciseModal = document.getElementById("exerciseModal");
+    const openExerciseBtn = document.getElementById("openExerciseModalBtn");
+    const closeExerciseBtn = document.getElementById("closeExerciseModal");
+
+    if (openExerciseBtn && exerciseModal && closeExerciseBtn) {
+      openExerciseBtn.onclick = () => {
+        exerciseModal.style.display = "block";
+      };
+        closeExerciseBtn.onclick = () => {
+          exerciseModal.style.display = "none";
+        };
+        window.onclick = (event) => {
+          if (event.target === exerciseModal) {
+            exerciseModal.style.display = "none";
+          }
+        };
+      } else {
+        console.warn("Modal elements not found.");
       }
-    };
-  }
-  });
+    });

01f52fe22e8d8e846e31a94c8089748e8fbe14f3
swimmingdiscount <23361098@student.uwa.edu.au>
2025-05-16 14:43:07 +0800

    updated JS to account for new hidden field

--- app/static/js/calorie_counter.js
+++ app/static/js/calorie_counter.js
@@ -1,29 +1,42 @@
-// Calorie Counter Modal (on calorie page not on dashboard)
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("Script is loaded");
-  
+  console.log("Script is loaded");
+
   const logModal = document.getElementById("countModal");
   const openLogModalBtn = document.getElementById("openCountModalBtn");
   const closeLogModalBtn = document.getElementById("closeCountModal");
-  
+
   flatpickr(".date-picker", {
-  enableTime: true,
-  dateFormat: "Y-m-d H:i",
+    enableTime: true,
+    dateFormat: "Y-m-d H:i",
   });
 
-  if (openCountModalBtn && countModal && closeCountModalBtn) {
-    openCountModalBtn.onclick = () => {
-      countModal.style.display = "block";
+  if (openLogModalBtn && logModal && closeLogModalBtn) {
+    openLogModalBtn.onclick = () => {
+      logModal.style.display = "block";
     };
-  
-    closeCountModalBtn.onclick = () => {
-      countModal.style.display = "none";
+
+    closeLogModalBtn.onclick = () => {
+      logModal.style.display = "none";
     };
-  
+
     window.onclick = function (event) {
-      if (event.target == countModal) {
-        countModal.style.display = "none";
+      if (event.target === logModal) {
+        logModal.style.display = "none";
       }
     };
   }
+
+  document.addEventListener("click", function (e) {
+    if (e.target.classList.contains("food-result")) {
+      const selected = e.target.dataset;
+
+      const idField = document.querySelector("input[name='selected_food_id']");
+      const fdcField = document.querySelector("input[name='fdc_id']");
+      const calField = document.querySelector("input[name='selected_food_cal']");
+
+      if (idField)  idField.value = selected.id || '';
+      if (fdcField) fdcField.value = selected.fdcId || '';
+      if (calField) calField.value = selected.calories || '';
+    }
   });
+});
